5a4d699870753d67a13348011f64a38c63407c09
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-11-08 15:31:56 +0100

    Add interactivity to component library VRTs

--- backstop_component_library/engine_scripts/chromy/onReady.js
+++ backstop_component_library/engine_scripts/chromy/onReady.js
@@ -1,5 +1,34 @@
 module.exports = function (chromy, scenario, vp) {
   console.log('SCENARIO > ' + scenario.label);
   require('./clickAndHoverHelper')(chromy, scenario);
-  // add more ready handlers here...
+
+  var scenarioLabel = scenario.label.toLowerCase()
+
+  if (scenarioLabel === 'autocomplete') {
+    chromy
+      .evaluate(() => {
+        var autocomplete = document.querySelector('#country-code-auto-complete')
+        autocomplete.value = 'united'
+        autocomplete.dispatchEvent(new Event('keyup'))
+      })
+  }
+
+  if (scenarioLabel === 'youtube-player') {
+    chromy.evaluate(function () {
+      var youTubePlayerContainerElement = document.querySelector('.youtube-player-container')
+      var maskingDivElement = document.createElement('div')
+
+      maskingDivElement.style.cssText = [
+        'position: absolute;',
+        'top: 0;',
+        'left: 0;',
+        'width: 100%;',
+        'height: 100%;',
+        'background-color: #000;',
+        'z-index: 1000;'
+      ].join('')
+
+      youTubePlayerContainerElement.insertBefore(maskingDivElement, youTubePlayerContainerElement.firstElementChild)
+    })
+  }
 };
